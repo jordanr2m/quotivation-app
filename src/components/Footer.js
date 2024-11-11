c6fff09e095c32bdcd18b2742797010df7674576
Jordan J <jordanr2m@gmail.com>
2024-11-11 14:34:49 -0800

    Updated Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className='wrapper'>&copy; 2023 Quotivation</footer>;
+  return (
+    <footer className='wrapper'>
+      &copy; {new Date().getFullYear()} || Coded by <a href="https://github.com/jordanr2m" target="blank">Jordan Jackson</a>
+    </footer>
+  );
 };
 
 export default Footer;

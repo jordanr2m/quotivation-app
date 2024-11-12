bfa64ebda31da98327418edfe0bb2128140795f9
Jordan J <jordanr2m@gmail.com>
2024-11-12 09:54:29 -0800

    Hooked up fetch request for API data.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Loader } from "react-feather";
 import "./App.css";
 
 function App() {
-  const quotesUrl =
-    "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+  const [quotes, setQuotes] = useState([]); // API data
+  const [isLoading, setIsLoading] = useState(false);
+  const quotesUrl = "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+
+  const fetchQuotes = async () => {
+    try {
+      setIsLoading(true);
+      const request = await fetch(quotesUrl);
+      const results = await request.json();
+      setQuotes(results);
+    } catch (err) {
+      console.log("Something went wrong", err);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
 
   return (
     <div className='App'>
       <Header />
-      <main></main>
+      <main>
+        {isLoading ? <Loader /> : JSON.stringify(quotes)}
+      </main>
       <Footer />
     </div>
   );

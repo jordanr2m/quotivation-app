a8b2b9e946eb0fb6b6306ccbbb390905a88e279e
Jordan J <jordanr2m@gmail.com>
2024-11-13 15:58:48 -0800

    Added Filter from react-feather.

--- src/components/quotes/CategoryForm.js
+++ src/components/quotes/CategoryForm.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import { Filter } from "react-feather";
 
 const CategoryForm = () => {
   return (
     <div className='category-form'>
       <form className='category-filter'>
+        <Filter />
         <label htmlFor='category'>Filter Quotes:</label>
         <select id='category' name='category'></select>
       </form>

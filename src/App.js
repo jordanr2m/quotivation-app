e410543651c0fc761b0e334555c36270163f55e1
Jordan J <jordanr2m@gmail.com>
2024-11-13 15:47:01 -0800

    Created CategoryForm component.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import "./App.css";
 function App() {
   const [quotes, setQuotes] = useState([]); // API data
   const [isLoading, setIsLoading] = useState(false);
+  const [category, setCategory] = useState("All");
   const quotesUrl = "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+  const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Empowerment"];
 
   const fetchQuotes = async () => {
     try {
---
+++ src/components/quotes/CategoryForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CategoryForm = () => {
+  return (
+    <div className='category-form'>
+      <form className='category-filter'>
+        <label htmlFor='category'>Filter Quotes:</label>
+        <select id='category' name='category'></select>
+      </form>
+    </div>
+  )
+}
+
+export default CategoryForm

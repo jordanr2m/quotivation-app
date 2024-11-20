5c56900a4cb41f754ad418f11b41fd0feec36e68
Jordan J <jordanr2m@gmail.com>
2024-11-20 10:39:12 -0800

    Added useEffect to remove popup message after a 1.5 second timeout.

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+
 const Message = ({ messageText, removeMessage }) => {
+    // Remove popup message after 1.5s
+    useEffect(() => {
+        const messageTimeout = window.setTimeout(() => {
+            removeMessage()
+        }, 1500);
+        // Cleanup function
+        return () => window.clearTimeout(messageTimeout);
+    }); // No dependency needed
+
     return (
         <div className="message">
             <p>{messageText}</p>
